3416e007806c34d223ae521036c32c78e1d1dfca
Aurélien <mwliad@gmail.com>
2021-09-11 15:16:38 +0200

    Add ghost details to page

--- scooby.js
+++ scooby.js
@@ -5,71 +5,80 @@ DarkReader.enable({
 });
 
 const scoobyApp = {
-  data() {
-    return {
-      clues: {
-        evidences: [],
-        unaffected: [],
-      },
-      cluesNames: ["EMF Level 5", "Ghost Orb", "Spirit Box", "Freezing Temperatures", "Ghost Writing", "Fingerprints", "DOTS Projector"],
-      ghosts: [],
-    };
-  },
-  computed: {
-    ghostsByClues() {
-        let suspects = this.ghosts;
-        this.clues.evidences.forEach(e => {
-            suspects = _.filter(suspects, function(g) { return _.some(g.clues, function(ev) { return ev === e})});
-        });
-        this.clues.unaffected.forEach(e => {
-            suspects = _.filter(suspects, function(g) { return !_.some(g.clues, function(ev) { return ev === e})});
-        })
-        return suspects;
+    data() {
+        return {
+            clues: {
+                evidences: [],
+                unaffected: [],
+            },
+            cluesNames: ["EMF Level 5", "Ghost Orb", "Spirit Box", "Freezing Temperatures", "Ghost Writing", "Fingerprints", "DOTS Projector"],
+            ghosts: [],
+            ghostDetails: false,
+            ghostTarget: {}
+        };
     },
-    dealBreakers() {
-        if (this.clues.evidences.length == 0 && this.clues.unaffected.length == 0) {
-            return this.cluesNames;
+    computed: {
+        ghostsByClues() {
+            let suspects = this.ghosts;
+            this.clues.evidences.forEach(e => {
+                suspects = _.filter(suspects, function (g) { return _.some(g.clues, function (ev) { return ev === e }) });
+            });
+            this.clues.unaffected.forEach(e => {
+                suspects = _.filter(suspects, function (g) { return !_.some(g.clues, function (ev) { return ev === e }) });
+            })
+            return suspects;
+        },
+        dealBreakers() {
+            if (this.clues.evidences.length == 0 && this.clues.unaffected.length == 0) {
+                return this.cluesNames;
+            }
+
+            let remainingClues = []
+
+            // Collect every remaining possible clues
+            _.each(this.ghostsByClues, function (e) {
+                remainingClues = _.concat(remainingClues, e.clues)
+            });
+            remainingClues = _.uniq(remainingClues)
+
+            // Exclude already collected clues
+            remainingClues = _.difference(remainingClues, this.clues.evidences)
+            remainingClues = _.difference(remainingClues, this.clues.unaffected)
+
+            return remainingClues
         }
-
-        let remainingClues = []
-
-        // Collect every remaining possible clues
-        _.each(this.ghostsByClues,function(e) {
-            remainingClues = _.concat(remainingClues, e.clues)
-        });
-        remainingClues = _.uniq(remainingClues)
-
-        // Exclude already collected clues
-        remainingClues = _.difference(remainingClues, this.clues.evidences)
-        remainingClues = _.difference(remainingClues, this.clues.unaffected)
-
-        return remainingClues
+    },
+    beforeMount() {
+        this.invokeGhosts();
+    },
+    methods: {
+        async invokeGhosts() {
+            const res = await fetch("./ghosts.json")
+            const data = await res.json();
+            this.ghosts = data.ghosts;
+        },
+        modifyEvidence(data) {
+            if (data.state == 0) {
+                this.clues.unaffected.push(data.evidenceName);
+                this.clues.evidences = _.reject(this.clues.evidences, function (e) { return e == data.evidenceName })
+            }
+            if (data.state == 1) {
+                this.clues.unaffected = _.reject(this.clues.unaffected, function (e) { return e == data.evidenceName })
+                this.clues.evidences = _.reject(this.clues.evidences, function (e) { return e == data.evidenceName })
+            }
+            if (data.state == 2) {
+                this.clues.evidences.push(data.evidenceName);
+                this.clues.unaffected = _.reject(this.clues.unaffected, function (e) { return e == data.evidenceName })
+            }
+        },
+        showGhostDetails(ghostName) {
+            var targetGhost = _.find(this.ghosts, function (g) { return g.name == ghostName });
+            if (targetGhost) {
+                this.ghostTarget = targetGhost;
+                this.ghostDetails = true;
+            }
+        }
     }
-  },
-  beforeMount() {
-      this.invokeGhosts();
-  },
-  methods: {
-      async invokeGhosts() {
-          const res = await fetch("./ghosts.json")
-          const data = await res.json();
-          this.ghosts = data.ghosts;
-      },
-      modifyEvidence(data) {
-          if (data.state == 0) {
-            this.clues.unaffected.push(data.evidenceName);
-            this.clues.evidences = _.reject(this.clues.evidences, function(e) { return e == data.evidenceName})
-          }
-          if (data.state == 1) {
-            this.clues.unaffected = _.reject(this.clues.unaffected, function(e) { return e == data.evidenceName})
-            this.clues.evidences = _.reject(this.clues.evidences, function(e) { return e == data.evidenceName})
-          } 
-          if (data.state == 2) {
-            this.clues.evidences.push(data.evidenceName);
-            this.clues.unaffected = _.reject(this.clues.unaffected, function(e) { return e == data.evidenceName})
-          } 
-      }
-  }
 };
 
 const app = Vue.createApp(scoobyApp)
@@ -83,7 +92,7 @@ app.component("evidence", {
     },
     methods: {
         emitClue(evidenceName, state) {
-            this.$emit('toggle-clue', {evidenceName: evidenceName, state: state})
+            this.$emit('toggle-clue', { evidenceName: evidenceName, state: state })
         }
     },
     template: `
